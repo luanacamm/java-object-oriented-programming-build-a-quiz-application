92816930eec146cba91e6ca10f00fd9b20e5d00f
luanacamm <luanascamara@gmail.com>
2018-08-20 00:25:44 -0300

    1st version

---
+++ src/ClassesAndObjects.java
@@ -0,0 +1,26 @@
+public class ClassesAndObjects {
+	
+	public static void main (String[] args) {
+		
+		Dog obj1 = new Dog();
+		obj1.bark();
+		obj1.walk();
+	}
+
+}
+
+class Dog {
+	
+	String breed;
+	String color;
+	
+	void bark() {
+		
+		System.out.println("Barks");
+	}
+	
+	void walk() {
+		
+		System.out.println("Walks");
+	}
+}
